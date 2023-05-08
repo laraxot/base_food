78bdb2cf8f36979d53e4c90f8c15bb3eb3959925
marco76tv <marco.sottana@gmail.com>
2023-05-08 15:52:08 +0200

    .

---
+++ composer_custom.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+php -r "unlink('composer.lock');"
+rm composer.lock
+rm package-lock.json
+
+#gestire FFMPEG
+php -d memory_limit=-1 composer.phar require -W pbmedia/laravel-ffmpeg
